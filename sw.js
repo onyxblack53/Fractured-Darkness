029057c3980d6f03e33f5acf65b1fdb3d0da6cec
onyxblack53 <8v5v6bp2hz@privaterelay.appleid.com>
2026-09-18 12:12:58 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-const CACHE="fractured-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="fractured-v02";const A=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-512.png"];self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)))});self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
